f81d404371d336951fd680ff58dec21ba0917a20
Alexey Vitvor <avitvor@gmail.com>
2020-09-10 10:52:38 +0300

    Update third lab

--- Lab3/server.js
+++ Lab3/server.js
@@ -1,6 +1,17 @@
 const http = require("http");
 const fs = require("fs");
+const url=require("url");
+const {
+    performance,
+    PerformanceObserver
+  } = require('perf_hooks');
 var status='norm';
+
+let factorial=(m)=>
+{
+    var n = parseInt(m);
+    return (n < 0 || m != n) ? NaN : (n == 0 ? 1 : n * factorial(n - 1));
+}
 
 process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', ()=>{
@@ -30,23 +41,61 @@ process.stdin.on('readable', ()=>{
 
 http.createServer(function(request, response){
     // получаем путь после слеша
-    const filePath = request.url.substr(1);
-    switch(filePath)
+    //const filePath = request.url.substr(1);
+    const urlRequest=url.parse(request.url, true);
+    switch(urlRequest.pathname.substr(1))
     {
         case 'html':
-            fs.access("D:\\3_Course\\CrossplatformApplications\\NodeLabs\\Lab3\\index.html", err => {
+            fs.access("index.html", err => {
                 // если произошла ошибка - отправляем статусный код 404
                 if(err){
                     response.statusCode = 404;
                     response.end("Resourse not found!");
                 }
                 else{
-                    fs.createReadStream("D:\\3_Course\\CrossplatformApplications\\NodeLabs\\Lab3\\index.html").pipe(response);
+                    fs.createReadStream("index.html").pipe(response);
                 }
               });
               break;
         case 'status':
             response.statusCode=200;
             response.end(status);
+        case 'fact':
+            if(request.method=='GET')
+            {
+                k=parseInt(urlRequest.query.k);
+                var time=performance.now();
+                var fact=factorial(k);
+                time=performance.now()-time;
+                time=parseInt(time*1000);
+                response.writeHead(200, {'Content-type':'application/json'});
+                response.end(JSON.stringify({t:time, k:k, fact:fact}));
+            }
+            else
+            {
+                response.writeHead(405, {'Content-type':'text/plain'});
+                response.end("Invalid http method");
+            }
+            break;
+        case 'factall':
+            if(request.method=='GET')
+            {
+                fs.access("factorial.html", err => {
+                    // если произошла ошибка - отправляем статусный код 404
+                    if(err){
+                        response.statusCode = 404;
+                        response.end("Resourse not found!");
+                    }
+                    else{
+                        fs.createReadStream("factorial.html").pipe(response);
+                    }
+                  });
+                break;
+            }
+            else
+            {
+                response.writeHead(405, {'Content-type':'text/plain'});
+                response.end("Invalid http method");
+            }
     }
 }).listen(3000, function(){console.log("Server started on 3000 port")});
